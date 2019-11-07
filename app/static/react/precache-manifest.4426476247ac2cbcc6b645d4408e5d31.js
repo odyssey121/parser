17b429d6f2ffbf61ba047b9f0639def2046ab98a
lesha Shirnin <odyssey121@mail.ru>
2019-11-07 12:36:53 +0300

    base set

---
+++ app/static/react/precache-manifest.4426476247ac2cbcc6b645d4408e5d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496e31cfc92468b172f7ccb56eabf415",
+    "url": "static/react/..\\..\\templates\\index.html"
+  },
+  {
+    "revision": "877df14d04cf006ebdff",
+    "url": "static/react/css/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e7b7b8aa793295ba2f92",
+    "url": "static/react/css/js/2.2c4adcce.chunk.js"
+  },
+  {
+    "revision": "877df14d04cf006ebdff",
+    "url": "static/react/css/js/main.b87562b6.chunk.js"
+  },
+  {
+    "revision": "0a35b47ca401048176db",
+    "url": "static/react/css/js/runtime-main.8707430d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "static/react/css/media/logo.25bf045c.svg"
+  }
+]);
