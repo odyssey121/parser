6ce82079ab7b13d08be7ba3d75b07f858f11a3f9
lesha Shirnin <odyssey121@mail.ru>
2019-11-22 14:42:46 +0300

    simple schedule

---
+++ cron.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from modules.siteAdapter import RssParser
+
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=10)
+def startSiteAdapter():
+    siteAdapter = RssParser()
+    siteAdapter.start()
